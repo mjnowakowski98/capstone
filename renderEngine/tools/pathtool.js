7f30fa23380318c0c22594ea6daac3af2d2cd02c
Nowakowski <mjnowakowski@labs.neit.edu>
2018-09-05 21:31:11 -0400

    Much workaround, very send help

--- renderEngine/tools/pathtool.js
+++ renderEngine/tools/pathtool.js
@@ -3,7 +3,6 @@ class PathTool extends Tool {
         super();
         this.m_toolName = "Rectangle";
 
-        this.m_newObj = null;
         this.m_points = null;
     }
 
@@ -11,9 +10,7 @@ class PathTool extends Tool {
         if(this.toolStateString === "toolReady") {
             this.m_points = new Array(Input.click);
             this.toolStateString = "toolActive";
-        }
-
-        if(this.toolStateString === "toolActive")
+        } else if(this.toolStateString === "toolActive")
             this.m_points.push(Input.click);
     }
 
@@ -28,28 +25,71 @@ class PathTool extends Tool {
     onToolEnding() {
         removeEventListener("keyup", this.endPath);
         var newObj = new Drawable("path", "Path");
+        newObj.fill = "#a3a3a3";
+        newObj.stroke = null;
+        newObj.points = new Array();
+        for(var i in this.m_points)
+            newObj.points.push(this.m_points[i]);
+
+        Anim.drawShape(renderer.anim.ctx, newObj);
+
+        var intersects = new Array();
+        for(var i = 0; i < newObj.points.length; i++) {
+            var p1 = Object.assign({}, newObj.points[i]), p2;
+            if(i + 1 === newObj.points.length) p2 = Object.assign({}, newObj.points[0]);
+            else p2 = Object.assign({}, newObj.points[i + 1]);
+
+            if(p1.x === p2.x || p1.y === p2.y) continue;
+
+            var tmp = p2.x;
+            p2.x = p1.x;
+            p1.x = tmp;
+
+            if(renderer.anim.ctx.isPointInPath(p1.x, p1.y))
+                intersects.push(p1);
+            else if(renderer.anim.ctx.isPointInPath(p2.x, p2.y))
+                intersects.push(p2);
+        }
+
+        console.log(intersects);
+
+        var intCenter = null,
+            pointCenter = null,
+            avgCenter = null;
+        var avgX = 0, avgY = 0;
+        for(var i in newObj.points) {
+            avgX += newObj.points[i].x;
+            avgY += newObj.points[i].y;
+        }
+        avgX /= intersects.length;
+        avgY /= intersects.length;
+        pointCenter = new Coord(avgX, avgY);
+
+        if(intersects.length > 0) {
+            avgX = 0;
+            avgY = 0;
+            for(var i in intersects) {
+                avgX += intersects[i].x;
+                avgY += intersects[i].y;
+            }
+
+            avgX /= intersects.length;
+            avgY /= intersects.length;
+            intCenter = new Coord(avgX, avgY);
+        }
+
+        if(intCenter && renderer.anim.ctx.isPointInPath(pointCenter.x, pointCenter.y))
+            avgCenter = new Coord((intCenter.x + pointCenter.x) / 2, (intCenter.y + pointCenter.y) / 2);
+
+        console.log(intCenter, pointCenter, avgCenter);
+
+        return;
+
         if(Tool.useFill) newObj.fill = Tool.fillColor;
         if(Tool.useStroke) {
             newObj.stroke = Tool.strokeColor;
             newObj.strokeWidth = Tool.strokeWidth;
         }
-
-        var xTotal = 0;
-        var yTotal = 0;
-        for(var i in this.m_points) {
-            xTotal += this.m_points[i].x;
-            yTotal += this.m_points[i].y;
-        }
-
-        var center = new Coord(xTotal / this.m_points.length, yTotal / this.m_points.length);
-        for(var i in this.m_points) {
-            this.m_points[i].x = this.m_points[i].x - center.x;
-            this.m_points[i].y = this.m_points[i].y - center.y;
-        }
-
-        newObj.points = new Array();
-        for(var i in this.m_points)
-            newObj.points[i] = Object.assign({}, this.m_points[i]);
 
         var objRef = renderer.anim.addObject(newObj);
         renderer.anim.addObjectToFrame(renderer.animFrame, "drawable", objRef, center.x, center.y);
